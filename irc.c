9d2c1f0ec47bf12792deabc0740a020be2287eb0
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-04-26 17:59:26 -0400

    irc.c: update

--- irc.c
+++ irc.c
@@ -18,17 +18,17 @@ FILE *server;
 int
 leirc_connect(lua_State *L)
 {
-	char *server = luaL_checkstring(L, 1);
-	char *port   = luaL_checkstring(L, 2);
+	char *host = luaL_checkstring(L, 1);
+	char *port = luaL_checkstring(L, 2);
 
-	int fd = dial(server, port);
+	int fd = dial(host, port);
 	if (!fd) {
 		return luaL_error(L, "error:"
 			"unable to resolve server: %s\n",
 			strerror(errno));
 	}
 
-	server = fdopen(dial(server, port), "r+");
+	server = fdopen(fd, "r+");
 
 	if (!server) {
 		return luaL_error(L, "error:"
