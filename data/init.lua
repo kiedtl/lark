05c733fc2fc0133a030bf903aae9aab4cd776fec
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-04-26 22:02:44 -0400

    init.lua: add

---
+++ data/init.lua
@@ -0,0 +1,20 @@
+function init()
+	leirc_connect("irc.freenode.net", 6667)
+
+	sleep(5)
+
+	leirc_write("NICK leirc_test")
+	leirc_write("JOIN #test")
+	leirc_write("PRIVMSG #test :test")
+
+	while true do
+		print(leirc_read())
+	end
+
+	leirc_disconnect()
+end
+
+function sleep(n)
+	local start = os.clock()
+	while os.clock() - start < n do end
+end
