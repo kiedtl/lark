93371e51ad76c1e7922c4f833dfee922dc0619b2
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-04-29 18:29:22 -0400

    handlers: add

--- data/core/handlers.lua
+++ data/core/handlers.lua
@@ -1,26 +1,53 @@
 -- TODO: move to config
 
-module "core"
+local api    = require('api')
+local core   = require('core')
+local parse_prefix = core.parse_prefix
+local config = require('config')
 
-handlers = {}
+local handlers = {}
+
+handlers["PONG"] = function(prefix, args)
+	-- do nothing, server recieved our ping
+	-- and responded.
+end
 
 handlers["PING"] = function(prefix, args)
-	leirc_write("PONG :" .. args)
+	-- so it seems we were inactive for some time,
+	-- server is pinging us to see if we're still there
+	api.sendf("PONG :%s", args)
 end
 
 handlers["PRIVMSG"] = function(prefix, args)
-	local user = parsePrefix(prefix)
+	local user = parse_prefix(prefix)
 	print(string.format("%14s %s", user, args))
 end
 
 handlers["NOTICE"] = function(prefix, args)
-	print(string.format("%14s %s", "NOTICE", args))
+	print(string.format("%14s %s", "NOTE", args))
 end
 
 handlers["JOIN"] = function(prefix, args)
-	print(string.format("%14s %s", "-->", args))
+	local user = parse_prefix(prefix)
+	print(string.format("%14s %s has joined", "-->", user))
 end
 
 handlers["PART"] = function(prefix, args)
-	print(string.format("%14s %s", "<--", args))
+	local user = parse_prefix(prefix)
+	print(string.format("%14s %s has left %s", "<--", user, args))
 end
+
+handlers["QUIT"] = function(prefix, args)
+	local user = parse_prefix(prefix)
+	print(string.format("%14s %s has quit (quit: %s) ", "<--", user, args))
+end
+
+handlers["NICK"] = function(prefix, args)
+	local user = parse_prefix(prefix)
+	print(string.format("%14s %s is now known as %s", user, args))
+	if user == config.username then
+		config.username = user
+	end
+end
+
+return handlers
--- data/core/init.lua
+++ data/core/init.lua
@@ -1,6 +1,5 @@
 local api    = require('api')
 local config = require('config')
-local curses = require('curses')
 
 local core = {}
 
@@ -15,13 +14,46 @@ function core.init()
 end
 
 function core.on_receive(cmd, pars, txt)
-	print(string.format(">< %s (%s): %s", cmd, pars, txt))
+	local handlers = require('core.handlers')
+	local handler = handlers[cmd]
+
+	if handler then
+		handler(pars, txt)
+	else
+		print(string.format(">< %s (%s): %s", cmd, pars, txt))
+	end
 end
 
 function core.on_error(err)
-	curses.endwin()
 	print(debug.traceback(err, 2))
 	os.exit(2)
+end
+
+function core.parse_prefix(prefix)
+	-- TODO: move to irc module
+	local user = {}
+
+	if prefix then
+		user.type,
+		user.nickname,
+		user.username,
+		user.hostname = prefix:match("^([%+@]*)(.+)!(.+)@(.+)$")
+	end
+
+	if user.type then
+		local type = {op = false, halfop = false, voice = false}
+
+		for c in user.type:gmatch(".") do
+			if     c == "@" then type.op = true
+			elseif c == "%" then type.halfop = true
+			elseif c == "+" then type.voice = true
+			end
+		end
+
+		user.type = type
+	end
+
+	return user
 end
 
 return core
